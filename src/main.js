1635ce40ce2749d1862e4e38585a61681ea7e7d2
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-09-23 19:05:33 -0500

    fix: layout html

--- src/main.js
+++ src/main.js
@@ -11,20 +11,27 @@ const anchoX = document.querySelector("#anchoX");
 
 // ---------- DATOS DE CUADRICULA - VALORES
 //  --- EJE X
-const gridIdCuadriculaX = document.querySelector("#gridIdX");
-const espaciadoXCuadriculaX = document.querySelector("#espaciadoX");
+const gridIdCuadriculaX = document.querySelectorAll(".gridIdX");
+const espaciadoXCuadriculaX = document.querySelectorAll(".espaciadoX");
 //  --- EJE Y
-const gridIdCuadriculaY = document.querySelector("#gridIdY");
-const espaciadoYCuadriculaY = document.querySelector("#espaciadoY");
+const gridIdCuadriculaY = document.querySelectorAll(".gridIdY");
+const espaciadoYCuadriculaY = document.querySelectorAll(".espaciadoY");
 
-// ----- NODES 
-const buttonSave = document.querySelector(".grabar-container");
-
+// ----- BUTTONS 
 const buttonPlusCuadriculaX = document.querySelector(".btn_plus_cuadricula-x");
 const buttonMinusCuadriculaX = document.querySelector(".btn_minus_cuadricula-x");
 
 const buttonPlusCuadriculaY = document.querySelector(".btn_plus_cuadricula-y");
 const buttonMinusCuadriculaY = document.querySelector(".btn_minus_cuadricula-y");
+
+const buttonSave = document.querySelector(".grabar-container");
+
+// -----NODES
+const contanierIdX = document.querySelector(".cuadricula_id-x");
+const contanierEspaciadoX = document.querySelector(".cuadricula_espaciado-x");
+
+const contanierIdY = document.querySelector(".cuadricula_id-y");
+const contanierEspaciadoY = document.querySelector(".cuadricula_espaciado-y");
 
 // FUNCTIONS
 function guardarDatos() {
@@ -34,16 +41,44 @@ function guardarDatos() {
 }
 
 function agregarLineaX() {
-    const campoAdcional = document.createElement("input");
-    campoAdcional.classList.add("gridIdX");
-    campoAdcional.setAttribute("type");
+    const campoAdcionalId = document.createElement("input");
+    campoAdcionalId.classList.add("gridIdX");
+    campoAdcionalId.setAttribute("type", "text");
+
+    const campoAdcionalEspaciado = document.createElement("input");
+    campoAdcionalEspaciado.classList.add("espaciadoX");
+    campoAdcionalEspaciado.setAttribute("type", "number");
+
+    contanierIdX.appendChild(campoAdcionalId);
+    contanierEspaciadoX.appendChild(campoAdcionalEspaciado)
 }
 
 function eliminarLineaX() {
+    console.log("here");
+    const gridX = Array.from(gridIdCuadriculaX);
+    console.log(gridX);
+    gridX.pop();
+    const cuadriculaX = Array.from(espaciadoXCuadriculaX);
+    cuadriculaX.pop();
 
+    const gridY = Array.from(gridIdCuadriculaY);
+    gridY.pop();
+    const cuadriculaY = Array.from(espaciadoYCuadriculaY);
+    cuadriculaY.pop();
 }
 
 function agregarLineaY() {
+    const campoAdcionalId = document.createElement("input");
+    campoAdcionalId.classList.add("gridIdY");
+    campoAdcionalId.setAttribute("type", "text");
+
+    const campoAdcionalEspaciado = document.createElement("input");
+    campoAdcionalEspaciado.classList.add("espaciadoY");
+    campoAdcionalEspaciado.setAttribute("type", "number");
+
+    contanierIdY.appendChild(campoAdcionalId);
+    contanierEspaciadoY.appendChild(campoAdcionalEspaciado)
+
 
 }
 
@@ -61,3 +96,29 @@ buttonMinusCuadriculaY.addEventListener("click", eliminarLineaY);
 buttonSave.addEventListener("click", guardarDatos)
 
 console.log(capacidadPortante);
+var trace1 = {
+    x: [],
+    y: [],
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [],
+    y: [],
+    type: 'scatter'
+  };
+  
+  var data = [trace1, trace2];
+  
+  var layout = {
+    xaxis: {
+      type: 'log',
+      autorange: true
+    },
+    yaxis: {
+      type: 'log',
+      autorange: true
+    }
+  };
+  
+  Plotly.newPlot('myDiv', data, layout);
