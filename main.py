4ed12d7c80d2c715e3adf7b4a3dfb9947aa538f6
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-09-30 18:38:59 -0500

    implmented cuadricula en el eje Z

--- main.py
+++ main.py
@@ -32,10 +32,13 @@ app.layout = html.Div([
                         'renamable': False
                     } for i in ['Factores', 'Valores']],
                     data=[
-                        {'Factores':"f'c Concreto", 'Valores':10},
-                        {'Factores':'Factor de Uso', 'Valores':10},
-                        {'Factores':'Factor de Zona', 'Valores':10},
-                        {'Factores':'Factor de suelo', 'Valores':10},
+                        {'Factores':"f'c Concreto", 'Valores':210},
+                        {'Factores':'Factor de Zona', 'Valores':1},
+                        {'Factores':'Factor de Uso', 'Valores':0.45},
+                        {'Factores':'Factor de suelo', 'Valores':1},
+                        {'Factores':'Coef. Basico de Reducción', 'Valores':8},
+                        {'Factores':'Tp', 'Valores':0.4},
+                        {'Factores':'TI', 'Valores':2.5},
                     ],
                     editable=True,
                     row_deletable=False,
@@ -279,6 +282,90 @@ app.layout = html.Div([
             ], style={
                 "margin": "40px 0px 50px 50px"
             }),
+
+            html.Div([
+                
+                html.Div([
+                    html.H2("Datos de cuadricula Z", style={
+                        "fontSize": "20px",
+                        "fontWeight": "700",
+                        "letterSpacing": "0",
+                        "lineHeight": "1.5em",
+                        "paddingBottom": "15px",
+                        "position": "relative",
+                        "color": "#f0a500",
+                        "fontFamily": "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif"
+                    })
+                ]),
+                
+                dash_table.DataTable(
+                    id='tabla-cuadricula-z',
+                    columns=[{
+                        'name': i,
+                        'id': i,
+                        'deletable': True,
+                        'renamable': True
+                    } for i in ['Grid', 'Espaciado Z']],
+                    data=[
+                        {i: 0 for i in ['Grid', 'Espaciado Z']}
+                        for j in range(3)
+
+                    ],
+                    editable=True,
+                    row_deletable=True,
+                    style_as_list_view=True,
+                    style_cell={
+                        'padding': '5px',
+                        "textAlign": "center"
+                        },
+                    style_header={
+                        'backgroundColor': '#9aa0a6',
+                        'fontWeight': 'bold',
+                        "color": "white",
+                        "textAlign": "center"
+                    }
+                    # style={
+                    #     "marginTop": "20px"
+                    # },
+                ),
+                
+                html.Button('Agregar fila', id='editing-rows-button-Z', n_clicks=0,
+                            style={
+                            "alignItems": "center",
+                            "appearance": "none",
+                            "backgroundColor": "#fff",
+                            "borderRadius": "24px",
+                            "borderStyle": "none",
+                            "boxShadow": "rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .14) 0 6px 10px 0,rgba(0, 0, 0, .12) 0 1px 18px 0",
+                            "boxSizing": "border-box",
+                            "color": "#3c4043",
+                            "cursor": "pointer",
+                            "display": "inline-flex",
+                            "fill": "currentcolor",
+                            "fontFamily": "Roboto,Arial,sans-serif",
+                            "fontSize": "14px",
+                            "fontWeight": "500",
+                            "height": "48px",
+                            "justifyContent": "center",
+                            "letterSpacing": ".25px",
+                            "lineHeight": "normal",
+                            "maxWidth": "100%",
+                            "overflow": "visible",
+                            "padding": "2px 24px",
+                            "position": "relative",
+                            "textAlign": "center",
+                            "textTransform": "none",
+                            "transition": "box-shadow 280ms cubic-bezier(.4, 0, .2, 1),opacity 15ms linear 30ms,transform 270ms cubic-bezie,(0, 0, .2, 1) 0ms",
+                            "userSelect": "none",
+                            "webkitUserSelect": "none",
+                            "touchAction": "manipulation",
+                            "width": "auto",
+                            "willChange": "transform,opacity",
+                            "zIndex": "0",
+                        }),
+            ], style={
+                "margin": "40px 0px 50px 50px"
+            })
         ], style={
             "display": "flex",
             "textAlign": "center",
@@ -379,15 +466,27 @@ def add_row(n_clicks, rows, columns):
     return rows
 
 @callback(
+    Output('tabla-cuadricula-z', 'data'),
+    Input('editing-rows-button-Z', 'n_clicks'),
+    State('tabla-cuadricula-z', 'data'),
+    State('tabla-cuadricula-z', 'columns'))
+def add_row(n_clicks, rows, columns):
+    if n_clicks > 0:
+        rows.append({c['id']: '' for c in columns})
+    return rows
+
+@callback(
     Output('container-button-basic', 'children'),
     Input('grabar-datos', 'n_clicks'),
     State('tabla-cuadricula-x', 'data'),
     State('tabla-cuadricula-y', 'data'),
     prevent_initial_call=True)
-def save_data(n_clicks, data_x, data_y):
+
+def save_data(n_clicks, data_x, data_y, data_z):
     
     df_data_x = pd.DataFrame(data_x)
     df_data_y = pd.DataFrame(data_y)
+    df_data_z = pd.DataFrame(data_z)
 
     print(df_data_x)
     print(df_data_y)
